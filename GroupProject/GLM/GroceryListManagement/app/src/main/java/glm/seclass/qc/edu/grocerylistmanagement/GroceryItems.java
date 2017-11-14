81cc27d0f26ed3da43c7637a7de05027191c52ca
delahozric <delahozric@gmail.com>
2017-11-13 19:59:55 -0500

    Committing edits to Bernice's code

--- GroupProject/GLM/GroceryListManagement/app/src/main/java/glm/seclass/qc/edu/grocerylistmanagement/GroceryItems.java
+++ GroupProject/GLM/GroceryListManagement/app/src/main/java/glm/seclass/qc/edu/grocerylistmanagement/GroceryItems.java
@@ -1,7 +1,13 @@
 package glm.seclass.qc.edu.grocerylistmanagement;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
+
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
@@ -13,17 +19,25 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+
 
 public class GroceryItems extends AppCompatActivity {
 
+
     ArrayList<Item> itemList = null;
     ItemAdapter adapter;
+
     ListView lv;
-    EditText searchItem;
-    Button addButton;
+    String searchItem;
+
+    SQLiteDatabase itemDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,57 +46,112 @@ public class GroceryItems extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        searchItem = (EditText) findViewById(R.id.searchItem);
-        addButton = (Button) findViewById(R.id.addButton);
+        itemList = new ArrayList<Item>();
+
+        itemDB = this.openOrCreateDatabase("Items",MODE_PRIVATE,null);
+        itemDB.execSQL("CREATE TABLE IF NOT EXISTS items (ItemID INTEGER PRIMARY KEY, itemName VARCHAR, itemType VARCHAR, price DOUBLE)");
+        itemDB.execSQL("CREATE TABLE IF NOT EXISTS listItems (ListItemID INTEGER PRIMARY KEY, ListID INTEGER, ItemID INTEGER, Quantity INTEGER)");
+
+        itemDB.execSQL("INSERT INTO items (itemName, itemType, price) VALUES ('apple','fruit',1.15)");
+        itemDB.execSQL("INSERT INTO items (itemName, itemType, price) VALUES ('orange','fruit',3.15)");
+        itemDB.execSQL("INSERT INTO items (itemName, itemType, price) VALUES ('pear','fruit',2.15)");
+        itemDB.execSQL("INSERT INTO items (itemName, itemType, price) VALUES ('cucumber','vegetable',.15)");
+        itemDB.execSQL("INSERT INTO items (itemName, itemType, price) VALUES ('lettuce','vegetable',3.15)");
+        itemDB.execSQL("INSERT INTO items (itemName, itemType, price) VALUES ('cabbage','vegetable',5.15)");
+        itemDB.execSQL("INSERT INTO items (itemName, itemType, price) VALUES ('bacon','meat',5.15)");
+        itemDB.execSQL("INSERT INTO items (itemName, itemType, price) VALUES ('prime rib','meat',7.15)");
+        itemDB.execSQL("INSERT INTO items (itemName, itemType, price) VALUES ('ground beef','meat',8.15)");
+        itemDB.execSQL("INSERT INTO items (itemName, itemType, price) VALUES ('chicken wing','meat',9.15)");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_grocery_items, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if(id == R.id.createItem){
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Search for item:");
+            final EditText input = new EditText(this);
+            builder.setView(input);
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Item item = new Item();
+                    searchItem = input.getText().toString();
+
+                    item.setName(searchItem); //setting name to item object
+                    itemList.add(item);
+
+                    lv = (ListView) findViewById(R.id.itemList);
+                    ItemAdapter adapter = new ItemAdapter(getApplicationContext(), R.layout.item_row, itemList);
+                    lv.setAdapter(adapter);
 
 
+                    // itemDB.execSQL("INSERT INTO items (itemName, itemType, price) VALUES ('" +newItemName+ "','" +newItemCategory+ "','"+newItemPrice+"')");
+                }
+            });
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+            builder.show();
 
-        addButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                Item item = new Item();
-                itemList = new ArrayList<Item>();
-
-                String sItem = searchItem.getText().toString();
-                item.setItem(sItem);
-                itemList.add(item);
-
-                lv = (ListView) findViewById(R.id.itemList);
-                //addItem();
-                ItemAdapter adapter = new ItemAdapter(getApplicationContext(), R.layout.item_row, itemList);
-                lv.setAdapter(adapter);
-
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 
+    public class Item {
+        private String name;
+
+        //empty constructor for item
+        public Item(){
+        }
+        //object constructor for item
+        public Item(String name){
+            this.name = name;
+        }
+        //set method for name
+        public void setName(String name) {
+            this.name = name;
+        }
+        //returns item name
+        public String getName() {
+            return name;
+        }
+    }
+
+    private boolean searchItem() {
+        boolean match = false;
+        String searchedName = "hi";
+        Cursor c = itemDB.rawQuery("SELECT * FROM items",null);
+        int nameIndex = c.getColumnIndex("itemName");
+        c.moveToFirst();
+
+        while(c != null) {
+            String comparisonName = c.getString(nameIndex);
+            if(searchedName.equals(comparisonName)) {
+                return true;
             }
-
-
-        });
-
-
-
-
-
+            else
+                c.moveToNext();
+        }
+        return false;
     }
-    //testing to see if an item could be added
-    private void addItem()
-    {
-        Item item = new Item();
-        item.setItem("Bananas");
-        itemList.add(item);
-    }
-
-    public class Item
-    {
-        private String item;
-
-        public void setItem(String id) {this.item = item;}
-
-        public String getItem() {return item;}
-
-    }
-
     public class ItemAdapter extends ArrayAdapter {
         private List<Item> itemList;
         private int resource;
@@ -106,15 +175,13 @@ public class GroceryItems extends AppCompatActivity {
             CheckBox checkBox;
             EditText quantity;
 
-
             itemName = (TextView) convertView.findViewById(R.id.itemName);
             checkBox = (CheckBox) convertView.findViewById(R.id.checkBox);
             quantity = (EditText) convertView.findViewById(R.id.quantity);
 
-            itemName.setText(itemList.get(position).getItem());
+            itemName.setText(searchItem);
 
             return convertView;
-
         }
     }
 
--- GroupProject/GLM/GroceryListManagement/app/src/main/java/glm/seclass/qc/edu/grocerylistmanagement/GroceryLists.java
+++ GroupProject/GLM/GroceryListManagement/app/src/main/java/glm/seclass/qc/edu/grocerylistmanagement/GroceryLists.java
@@ -1,11 +1,16 @@
 package glm.seclass.qc.edu.grocerylistmanagement;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
+
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,15 +18,19 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Collections;
 
+
 public class GroceryLists extends AppCompatActivity {
 
+    SQLiteDatabase listDB;
     ArrayList<String> groceryList = null;
     ArrayAdapter<String> adapter = null;
     ListView lv = null;
+    int pos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +39,14 @@ public class GroceryLists extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        listDB = this.openOrCreateDatabase("Lists",MODE_PRIVATE,null);
+        listDB.execSQL("CREATE TABLE IF NOT EXISTS lists (ListID INTEGER PRIMARY KEY, listName VARCHAR)");
+        listDB.execSQL("CREATE TABLE IF NOT EXISTS customers (CustomerID INTEGER PRIMARY KEY, firstName VARCHAR, lastName VARCHAR, email VARCHAR, password VARCHAR)");
+        listDB.execSQL("CREATE TABLE IF NOT EXISTS customerLists (CustomerListID INTEGER PRIMARY KEY, listID INTEGER, CustomerID INTEGER)");
+
+        //placeholder - once we have a login screen, we can make this more official
+        listDB.execSQL("INSERT INTO customers (firstName, lastName, email, password) VALUES ('John','Smith','johnsmith@gmail.com','John1234')");
+
         groceryList = new ArrayList<>();
 
         adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, groceryList);
@@ -37,6 +54,7 @@ public class GroceryLists extends AppCompatActivity {
         lv = (ListView) findViewById(R.id.groceryList);
         lv.setAdapter(adapter);
 
+        //Access to grocery list contents
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapter, View view, int position, long arg) {
@@ -45,6 +63,62 @@ public class GroceryLists extends AppCompatActivity {
             }
         });
 
+        lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //Sets position
+                pos = i;
+                return false;
+            }
+        });
+        //Register to the context Menu
+        this.registerForContextMenu(lv);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        if(v.getId() == R.id.groceryList)
+            this.getMenuInflater().inflate(R.menu.context_menu, menu);
+
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        int selectedItemId = item.getItemId();
+
+        //Access to the list view
+        switch(selectedItemId) {
+            case R.id.deleteList:
+                //remove
+                adapter.remove(groceryList.get(pos));
+                String nameToDelete = "yoyo";
+                listDB.execSQL("DELETE FROM lists where lists.listName = '"+ nameToDelete +"'  ");
+                //refresh
+                adapter.notifyDataSetChanged();
+
+                break;
+            case R.id.renameList:
+                adapter.remove(groceryList.get(pos));
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Edit name:");
+                final EditText input = new EditText(this);
+                String newName = input.getText().toString();
+                String oldName = "yoyo";
+
+                builder.setView(input);
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        adapter.insert(input.getText().toString(), pos);
+                        Collections.sort(groceryList);
+                    }
+                });
+                builder.show();
+                listDB.execSQL("UPDATE lists SET listName = '"+ newName +"' WHERE listName = '"+ oldName +"' ");
+                break;
+        }
+        return super.onContextItemSelected(item);
     }
 
     @Override
@@ -69,9 +143,14 @@ public class GroceryLists extends AppCompatActivity {
             builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
-                    groceryList.add(input.getText().toString());
+                    String DBhelper = input.getText().toString();
+
+                    groceryList.add(DBhelper);
                     Collections.sort(groceryList);
                     lv.setAdapter(adapter);
+
+                    //insert into database here
+                    listDB.execSQL("INSERT INTO lists (listName) VALUES ('" + DBhelper + "')");
                 }
             });
             builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -83,18 +162,6 @@ public class GroceryLists extends AppCompatActivity {
             builder.show();
             return true;
         }
-
-        if (id == R.id.renameList) {
-            //do something
-
-            return true;
-        }
-
-        if (id == R.id.deleteList) {
-            //do something
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
